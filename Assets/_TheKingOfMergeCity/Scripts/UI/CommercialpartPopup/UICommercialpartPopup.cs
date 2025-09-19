7888288e6b22d1446ed92953b69cd02e4cf22da2
caolong201 <126745101+caolong201@users.noreply.github.com>
2025-09-19 17:42:19 +0700

    updateBonusItemChance

---
+++ Assets/_TheKingOfMergeCity/Scripts/UI/CommercialpartPopup/UICommercialpartPopup.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using TheKingOfMergeCity;
+using TheKingOfMergeCity.Enum;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICommercialpartPopup : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private Button buttonGem;     
+    [SerializeField] private Button buttonClose;  
+    [SerializeField] private RectTransform board;  
+    private int cost = 4;
+    void Start()
+    {
+        buttonGem.onClick.AddListener(OnClickBuyWithGem);
+        buttonClose.onClick.AddListener(CloseCommercial);
+        board.localScale = Vector3.zero;
+        board.DOScale(1f, 0.3f).SetEase(Ease.OutBack);
+    }
+
+    private void OnClickBuyWithGem()
+    {
+        int currentGem = UserManager.Instance.GetCurrencyBalance(CurrencyType.Gem);
+
+        if (currentGem >= cost)
+        {           
+            UserManager.Instance.AddCurrencyAmount(CurrencyType.Gem, -cost, true, true);   
+           UserManager.Instance.GetCurrencyBalance(CurrencyType.Gem);        
+            CloseCommercial();
+        }
+        else
+        {
+            Debug.Log("Không đủ gem!");          
+        }
+    }
+
+    public void CloseCommercial()
+    {
+      
+        board.DOScale(0f, 0.25f).SetEase(Ease.InBack).OnComplete(() =>
+        {
+            Destroy(gameObject);
+        });
+    }
+}
